08e98079add67192f3bc7182da08f8a9e8666239
Benjamin Chen <benchen4998@gmail.com>
2019-04-05 02:28:09 -0400

    assignment 6

--- assignments/6_Forensics_II/stub.py
+++ assignments/6_Forensics_II/stub.py
@@ -8,6 +8,9 @@ import struct
 def bork(msg):
     sys.exit(msg)
 
+def is_ascii(s)
+	return all(ord(c) < 128 for c in s)
+
 
 # Some constants. You shouldn't need to change these.
 MAGIC = 0x8BADF00D
@@ -25,6 +28,7 @@ with open(sys.argv[1], 'rb') as fpff:
 # Hint: you might find it easier to use an index/offset variable than
 # hardcoding ranges like 0:8
 magic, version = struct.unpack("<LL", data[0:8])
+ctr = 8;
 
 if magic != MAGIC:
     bork("Bad magic! Got %s, expected %s" % (hex(magic), hex(MAGIC)))
@@ -32,11 +36,65 @@ if magic != MAGIC:
 if version != VERSION:
     bork("Bad version! Got %d, expected %d" % (int(version), int(VERSION)))
 
+timestamp = struct.unpack("<L",data[ctr:ctr+4])
+ctr = ctr+4
+
+author = struct.unpack("<s",data[ctr:ctr+8])
+author.ljust(8,'0x0')
+ctr = ctr + 8
+
+section = struct.unpack("<d",data[ctr,ctr+4])
+ctr = ctr + 4
+
+if int(section) <= 0:
+	bork("Bad section, must be greater than 0")
+
 print("------- HEADER -------")
 print("MAGIC: %s" % hex(magic))
 print("VERSION: %d" % int(version))
+print("TIMESTAMP: %d" % int(timestamp))
+print("AUTHOR: %s" % str(author))
+print("SECTION COUNT: %d" % int(section))
+
 
 # We've parsed the magic and version out for you, but you're responsible for
 # the rest of the header and the actual FPFF body. Good luck!
 
 print("-------  BODY  -------")
+
+ctr = sizeoff(header)
+
+for x in range(int(section))
+	stype = struct.unpack("<h",data[ctr,ctr+4])
+	ctr = ctr + 4
+	slen = struct.unpack("<d",data[ctr,ctr+4])
+	ctr = ctr + 4
+	svalue = 0
+
+	if(int(slen) > 0):
+		svalue = struct.unpack("=s",data[ctr,ctr+slen])
+
+	#ASCII encode
+	if(hex(stype) == "0x1"):
+		if(is_ascii(svalue)):
+			print("Section Type: SECTION_ASCII")
+			print("Section Length: %d" % int(slen))
+			print("Section Value: %s" % svalue)
+		else:
+			bork("Bad Section")
+
+	if(hex(stype) == "0x2"):
+		try:
+			svalue.decode('utf-8')
+			print("Section Type: SECTION_AUTF8")
+			print("Section Length: %d" % int(slen))
+			print("Section Value: %s" % svalue)
+		except UnicodeError:
+			print("Bad Section")
+	if(hex(stype) == "0x3"):
+		
+
+
+
+
+	
--- assignments/6_Forensics_II/writeup/README.md
+++ assignments/6_Forensics_II/writeup/README.md
@@ -1,17 +1,33 @@
 # Writeup 6 - Forensics
 
-Name: *PUT YOUR NAME HERE*
-Section: *PUT YOUR SECTION NUMBER HERE*
+Name: Benjamin Chen
+Section: 0201
 
 I pledge on my honor that I have not given or received any unauthorized assistance on this assignment or examination.
 
-Digital acknowledgement: *PUT YOUR NAME HERE*
+Digital acknowledgement: Benjamin Chen
 
 ## Assignment Writeup
 
 ### Part 1 (45 Pts)
 
-*Replace this text with your repsonse to our prompt for part 1!*
+1) 142.93.136.81
+
+2) TCP reset attack
+
+3) 159.203.113.181, the IP address is from Clifton, NJ.
+
+4) Attackers are stealing from Port 20
+
+5a) It is a JPG file
+
+5c) 2018:12:23 17:16:24
+
+5d) Apple iPhone 8
+
+6) greetz.fpff
+
+7) I would recommend using IPsec in order to authenticate and encrypt incoming IP packets to prevent an attacker from using a TCP reset attack
 
 ### Part 2 (55 Pts)
 
